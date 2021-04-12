aba34d86f570c7c0a9e059027da165c695a78f35
samjo4788 <78034793+samjo4788@users.noreply.github.com>
2021-04-12 14:06:00 -0400

    week 11 and FB edits
    created week 11 and edited final project page, created sub pages for final project.

---
+++ Week11/p5serialbridge/sketch.js
@@ -0,0 +1,58 @@
+let serial;          // variable to hold an instance of the serialport library
+let portName = '/dev/tty.usbmodem14201';  // fill in your serial port name here
+let inData; 
+let xPos = 0; 
+ 
+function setup() {
+  createCanvas(400, 300);
+  background(0x08, 0x16, 0x40);
+  serial = new p5.SerialPort();       // make a new instance of the serialport library
+
+  serial.on('connected', serverConnected); // callback for connecting to the server
+  serial.on('open', portOpen);        // callback for the port opening
+  serial.on('data', serialEvent);     // callback for when new data arrives
+  serial.on('error', serialError);    // callback for errors
+  serial.on('close', portClose);      // callback for the port closing
+ 
+  serial.list();                      // list the serial ports
+  serial.open(portName);              // open a serial port
+}
+
+function serverConnected() {
+  console.log('connected to server.');
+}
+ 
+function portOpen() {
+  console.log('the serial port opened.')
+}
+ 
+function serialEvent() {
+  inData = Number(serial.read());
+}
+ 
+function serialError(err) {
+  console.log('Something went wrong with the serial port. ' + err);
+}
+ 
+function portClose() {
+  console.log('The serial port closed.');
+}
+function draw() {
+  graphData(inData);
+}
+function graphData(newData) {
+  // map the range of the input to the window height:
+  var yPos = map(newData, 0, 255, 0, height);
+  // draw the line in a pretty color:
+  stroke(0xA8, 0xD9, 0xA7);
+  line(xPos, height, xPos, height - yPos);
+  // at the edge of the screen, go back to the beginning:
+  if (xPos >= width) {
+    xPos = 0;
+    // clear the screen by resetting the background:
+    background(0x08, 0x16, 0x40);
+  } else {
+    // increment the horizontal position for the next reading:
+    xPos++;
+  }
+}
